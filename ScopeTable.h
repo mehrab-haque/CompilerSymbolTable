efece88db2ba44682e271f1d3a1fe8cb19aaf06e
mehrab-haque <mehrab.haque.0001@gmail.com>
2022-05-23 19:06:13 +0600

    SymbolTable class instantiated

---
+++ ScopeTable.h
@@ -0,0 +1,11 @@
+#ifndef SCOPETABLE_H
+#define SCOPETABLE_H
+
+#include "SymbolInfo.h"
+
+class ScopeTable{
+	int BUCKET_SIZE;
+	SymbolInfo *symbols;
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "SymbolInfo.h"
+#include "ScopeTable.h"
+
 using namespace std;
 
 int main(){
