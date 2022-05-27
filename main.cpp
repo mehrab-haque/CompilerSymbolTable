748aa7c924fecc2407323f0e1b7b500d0d9d6df3
mehrab-haque <mehrab.haque.0001@gmail.com>
2022-05-27 11:46:29 +0600

    input conditions done

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 #include <string>
 #include <fstream>
+#include "SymbolTable.h"
 
 using namespace std;
 
@@ -10,7 +11,11 @@ int main(){
 	string line;
 	
 	ifstream infile("input.txt");
-	bool isFirstLine=false;
+	bool isFirstLine=true;
+	
+	SymbolTable *symbolTable;
+	
+	
 	
 	while (getline(infile, line))
 	{
@@ -19,21 +24,39 @@ int main(){
 	    	isFirstLine=false;
 	    	int bucket_size;
 	    	iss>>bucket_size;
-	    	cout<<bucket_size<<endl;
+	    	symbolTable=new SymbolTable(bucket_size);
 		}else{
 			string command;
 		    iss>>command;
 		    if(command.compare("I")==0){
 		    	string name,type;
 		    	iss>>name>>type;
-		    	cout<<"Insert "<<name<<' '<<type<<endl;
+		    	symbolTable->insertSymbol(name,type);
 			}
 			else if(command.compare("L")==0){
 		    	string name;
 		    	iss>>name;
-		    	cout<<"Lookup "<<name<<endl;
+		    	symbolTable->lookup(name);
 			}
-		    
+			else if(command.compare("P")==0){
+		    	string printType;
+		    	iss>>printType;
+		    	if(printType.compare("A")==0)
+		    		symbolTable->printAllScopes();
+		    	else if(printType.compare("C")==0) 
+					symbolTable->printCurrentScope();
+			}
+			else if(command.compare("D")==0){
+		    	string name;
+		    	iss>>name;
+		    	symbolTable->removeSymbol(name);
+			}
+			else if(command.compare("S")==0){
+		    	symbolTable->enterScope();
+			}
+			else if(command.compare("E")==0){
+		    	symbolTable->exitScope();
+			}
 		}
 	}
 		
